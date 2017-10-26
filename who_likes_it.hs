a3f857829a529367069c35056bc73bda87736ef6
yann <me.yan.xia@qq.com>
2017-10-26 16:09:59 +0800

    Add who like it

---
+++ who_likes_it.hs
@@ -0,0 +1,8 @@
+likes :: [String] -> String
+likes [] = "no one likes this"
+likes (x:[]) = x ++ " likes this"
+likes (x:y:[]) = x ++ " and " ++ y ++  " like this"
+likes (x:y:xs) 
+    | length xs > 1 = x ++ ", " ++ y ++ " and " ++ (show $ length xs) ++  " others like this"
+    | otherwise = x ++ ", " ++ y ++ " and " ++ xs!!0 ++  " like this"
+        
