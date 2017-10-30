cbad571c8bba97d51661d1cd9dc2f7cf351604e8
yann <me.yan.xia@qq.com>
2017-10-30 22:04:39 +0800

    Add Hlint

--- primes_in_numbers.hs
+++ primes_in_numbers.hs
@@ -1,26 +1,20 @@
 import Data.List
 
-
 main :: IO ()
-main = do
-    print $ prime_factors 8612
+main = print $ primeFactors 8612
 
+primeFactors :: Integer -> String  
+primeFactors n = primeFactorsString $ group $ primeFactorsNums n 2
 
-prime_factors :: Integer -> String  
-prime_factors n = prime_factors_string $ group $ prime_factors_nums n 2
+primeFactorsString :: [[Integer]] -> String
+primeFactorsString [] = ""
+primeFactorsString (x:xs) 
+    | length x == 1 = "(" ++ show (head x) ++ ")" ++ primeFactorsString xs
+    | otherwise = "(" ++ show (head x) ++ "**" ++ show (length x) ++ ")" ++ primeFactorsString xs
 
-
-prime_factors_string :: [[Integer]] -> String
-prime_factors_string [] = ""
-prime_factors_string (x:xs) 
-    | length x == 1 = "(" ++ show (x !! 0) ++ ")" ++ (prime_factors_string xs)
-    | otherwise = "(" ++ show (x !! 0) ++ "**" ++ show (length x) ++ ")" ++ (prime_factors_string xs)
-
-
-
-prime_factors_nums :: Integer -> Integer -> [Integer]
-prime_factors_nums n m 
+primeFactorsNums :: Integer -> Integer -> [Integer]
+primeFactorsNums n m 
     | n < m = []
-    | n >= m && ((mod n m) == 0) = [m] ++ (prime_factors_nums (div n m) m)
-    | n >= m = prime_factors_nums n (m + 1)
+    | n >= m && (mod n m == 0) = m : primeFactorsNums (div n m) m
+    | n >= m = primeFactorsNums n (m + 1)
     | otherwise = []
--- sort_the_odd.hs
+++ sort_the_odd.hs
@@ -7,5 +7,5 @@ sortArray xs = replaceOdd xs oddSortList
 replaceOdd :: [Int] -> [Int] -> [Int]
 replaceOdd xs [] = xs
 replaceOdd (x:xs) os
-  | odd x = [head os] ++ (replaceOdd xs (tail os))
-  | otherwise = [ x ] ++ (replaceOdd xs os)
+  | odd x = head os : replaceOdd xs (tail os)
+  | otherwise = x : replaceOdd xs os
